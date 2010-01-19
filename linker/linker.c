3cab22c8cf0dcf30718a1452ce9cbb637876cea3
Min-su, Kim <min-su.kim@windriver.com>
2010-01-19 10:05:33 +0900

    linker: fix the undefined weak symbols issue

--- linker/linker.c
+++ linker/linker.c
@@ -471,7 +471,7 @@ _do_lookup(soinfo *si, const char *name, unsigned *base)
             DEBUG("%5d %s: looking up %s in %s\n",
                   pid, si->name, name, lsi->name);
             s = _do_lookup_in_so(lsi, name, &elf_hash);
-            if(s != NULL)
+            if ((s != NULL) && (s->st_shndx != SHN_UNDEF))
                 goto done;
         }
     }
