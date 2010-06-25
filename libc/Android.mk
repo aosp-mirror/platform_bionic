4f086aeb4aa06e13079b7fec71a8178ceeacf318
Matt Fischer <matt.fischer@garmin.com>
2010-06-25 14:36:39 -0500

    Implemented pthread_atfork()
    Change-Id: Ie6c0bf593315d3507b3c4a6c8903a74a1fa053db

--- libc/Android.mk
+++ libc/Android.mk
@@ -324,6 +324,7 @@ libc_common_src_files += \
 # up any thumb code.
 libc_common_src_files += \
 	bionic/pthread.c.arm \
+	bionic/pthread-atfork.c.arm \
 	bionic/pthread-timers.c.arm \
 	bionic/ptrace.c.arm
 
@@ -358,6 +359,7 @@ libc_common_src_files += \
 	arch-x86/string/strncmp_wrapper.S \
 	arch-x86/string/strlen.S \
 	bionic/pthread.c \
+	bionic/pthread-atfork.c \
 	bionic/pthread-timers.c \
 	bionic/ptrace.c
 
@@ -394,6 +396,7 @@ libc_common_src_files += \
 	string/memcmp.c \
 	string/strlen.c \
 	bionic/pthread.c \
+	bionic/pthread-atfork.c \
 	bionic/pthread-timers.c \
 	bionic/ptrace.c \
 	unistd/socketcalls.c
