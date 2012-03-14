21eab513e7eec280a7a8bcb9482a1a8b61e59442
Ben Cheng <bccheng@google.com>
2012-03-13 23:04:57 -0700

    New additions/bug fixes required/found when porting perf.
    New functions:
    	tfind
    	tsearch
    	tdelete
    	twalk
    	tdestroy (GNU extension)
    
    Bug fix: the current implementation for realpath would crash
    	if the second argument (resolved_path) is NULL.
    
    New headers:
    	ar.h
    	search.h
    
    Change-Id: Ib6c1e42fc186a6d597a6e5a9692b16acaa155804

---
+++ libc/bionic/tdestroy.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2012, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define _SEARCH_PRIVATE
+#include <search.h>
+#include <stdlib.h>
+
+/* destroy a tree and free all allocated resources */
+void
+tdestroy(void *root, void (*destroy_func)(void *))
+{
+    node_t *root_node = (node_t *) root;
+    if (root_node == NULL) return;
+    if (root_node->llink)
+        tdestroy(root_node->llink, destroy_func);
+    if (root_node->rlink)
+        tdestroy(root_node->rlink, destroy_func);
+    (*destroy_func)(root_node->key);
+    free(root);
+}
