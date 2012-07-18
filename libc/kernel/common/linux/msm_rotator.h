2730674ca991b49e036f2aba9beacc9f4cf5fe94
Naseer Ahmed <naseer@codeaurora.org>
2012-07-18 13:47:28 -0700

    bionic: Update MDP and rotator headers
    Change-Id: I585e07b004dfa6b5113050211ca67e4c057f5531

--- libc/kernel/common/linux/msm_rotator.h
+++ libc/kernel/common/linux/msm_rotator.h
@@ -46,28 +46,31 @@ struct msm_rotator_img_info {
  int enable;
  unsigned int downscale_ratio;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ unsigned int secure;
 };
 struct msm_rotator_data_info {
  int session_id;
- struct msmfb_data src;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ struct msmfb_data src;
  struct msmfb_data dst;
  unsigned int version_key;
  struct msmfb_data src_chroma;
- struct msmfb_data dst_chroma;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ struct msmfb_data dst_chroma;
 };
 struct msm_rot_clocks {
  const char *clk_name;
- enum rotator_clk_type clk_type;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ enum rotator_clk_type clk_type;
  unsigned int clk_rate;
 };
 struct msm_rotator_platform_data {
- unsigned int number_of_clocks;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ unsigned int number_of_clocks;
  unsigned int hardware_version_number;
  struct msm_rot_clocks *rotator_clks;
+ char rot_iommu_split_domain;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 };
 #endif
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+
