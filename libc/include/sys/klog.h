e249b059637b49a285ed9f58a2a18bfd054e5d95
Elliott Hughes <enh@google.com>
2012-09-14 15:42:30 -0700

    Fix <sys/klog.h> to correspond to the kernel operations.
    This makes the constants correspond to those in the Linux kernel's
    include/linux/syslog.h, but keeping our old badly-named constants for
    source compatibility.
    
    Change-Id: Ia47d1299205754cbfffd29ed48b497b53d1edaae

--- libc/include/sys/klog.h
+++ libc/include/sys/klog.h
@@ -32,17 +32,25 @@
 
 __BEGIN_DECLS
 
-#define KLOG_CLOSE      0
-#define KLOG_OPEN       1
-#define KLOG_READ       2
-#define KLOG_READ_ALL   3
-#define KLOG_READ_CLEAR 4
-#define KLOG_CLEAR      5
-#define KLOG_DISABLE    6
-#define KLOG_ENABLE     7
-#define KLOG_SETLEVEL   8
+/* These correspond to the kernel's SYSLOG_ACTION_whatever constants. */
+#define KLOG_CLOSE         0
+#define KLOG_OPEN          1
+#define KLOG_READ          2
+#define KLOG_READ_ALL      3
+#define KLOG_READ_CLEAR    4
+#define KLOG_CLEAR         5
+#define KLOG_CONSOLE_OFF   6
+#define KLOG_CONSOLE_ON    7
+#define KLOG_CONSOLE_LEVEL 8
+#define KLOG_SIZE_UNREAD   9
+#define KLOG_SIZE_BUFFER   10
+
+/* These are deprecated names that were used in earlier bionic releases. Do not use. */
+#define KLOG_DISABLE 6
+#define KLOG_ENABLE 7
+#define KLOG_SETLEVEL 8
 #define KLOG_UNREADSIZE 9
-#define KLOG_WRITE      10
+#define KLOG_WRITE 10
 
 extern int klogctl(int, char *, int);
 
