e0175ca7e440a87e9d03f249fc8f210063df7908
Elliott Hughes <enh@google.com>
2013-03-14 14:38:08 -0700

    Don't search off the end of the index for bad Olson ids.
    In the old code, the index was a file to itself, so it made sense to
    read until you hit the end of the file. In the new code, the index is
    followed by hundreds of KiB of data, so we need to just search the
    index.
    
    Bug: 8368791
    Change-Id: Icf5f8b5516cf3a93679fa849c9f6cd1cb100e0f1

---
+++ tests/time_test.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <sys/cdefs.h>
+#include <features.h>
+#include <gtest/gtest.h>
+
+#include <time.h>
+
+#ifdef __BIONIC__ // mktime_tz is a bionic extension.
+#include <libc/private/bionic_time.h>
+TEST(time, mktime_tz) {
+  struct tm epoch;
+  memset(&epoch, 0, sizeof(tm));
+  epoch.tm_year = 1970 - 1900;
+  epoch.tm_mon = 1;
+  epoch.tm_mday = 1;
+
+  // Alphabetically first. Coincidentally equivalent to UTC.
+  ASSERT_EQ(2678400, mktime_tz(&epoch, "Africa/Abidjan"));
+
+  // Alphabetically last. Coincidentally equivalent to UTC.
+  ASSERT_EQ(2678400, mktime_tz(&epoch, "Zulu"));
+
+  // Somewhere in the middle, not UTC.
+  ASSERT_EQ(2707200, mktime_tz(&epoch, "America/Los_Angeles"));
+
+  // Missing. Falls back to UTC.
+  ASSERT_EQ(2678400, mktime_tz(&epoch, "PST"));
+}
+#endif
