25a87f7641f066437d15c6d3148d08a6ab9be231
David 'Digit' Turner <digit@android.com>
2013-05-23 10:02:02 +0200

    Fix declaration of malloc_usable_size()
    The function should take a 'const void*' parameter, instead of 'void*'.
    Note that the implementation in upstream-dlmalloc/malloc.c already does
    this.
    
    For context, see http://b.android.com/55725
    
    Change-Id: Iefd55cdb8996699189e0545f9195972490306227

--- libc/bionic/malloc_debug_common.cpp
+++ libc/bionic/malloc_debug_common.cpp
@@ -190,7 +190,7 @@ extern "C" struct mallinfo mallinfo() {
     return dlmallinfo();
 }
 
-extern "C" size_t malloc_usable_size(void* mem) {
+extern "C" size_t malloc_usable_size(const void* mem) {
     return dlmalloc_usable_size(mem);
 }
 
