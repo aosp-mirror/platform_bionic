0aa9b52efa75b4c8bbdc12845434e25e457ea57b
Christopher Ferris <cferris@google.com>
2013-07-10 14:31:03 -0700

    Add new optimized strlen for arm.
    This optimized version is primarily targeted at cortex-a15.
    
    Tested on all nexus devices using the system/extras/libc_test strlen test.
    Tested alignments from 1 to 32 that are powers of 2.
    Tested that strlen does not cross page boundaries at all alignments.
    
    Speed improvements listed below:
    
    cortex-a15
    - Sizes >= 32 bytes, ~75% improvement.
    - Sizes >= 1024 bytes, ~250% improvement.
    
    cortex-a9
    - Sizes >= 32 bytes, ~75% improvement.
    - Sizes >= 1024 bytes, ~85% improvement.
    
    krait
    - Sizes >= 32 bytes, ~95% improvement.
    - Sizes >= 1024 bytes, ~160% improvement.
    
    Merge from internal master.
    
    (cherry-picked from 2fc071797743b88a9a47427d46baed7c7b24f4d2)
    
    Change-Id: I1ceceb4e745fd68e9d946f96d1d42e0cdaff6ccf

--- libc/arch-arm/generic/generic.mk
+++ libc/arch-arm/generic/generic.mk
@@ -1,3 +1,4 @@
 $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/generic/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/generic/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/generic/bionic/strcmp.S)
+$(call libc-add-cpu-variant-src,STRLEN,arch-arm/generic/bionic/strlen.c)
