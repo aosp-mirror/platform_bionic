1d13c64d781a87dfa3f01e5c179bfa36748786af
Elliott Hughes <enh@google.com>
2013-09-23 16:02:39 -0700

    Fix %hhd formats in the printf family.
    Found by adapting the simple unit tests for libc logging to test
    snprintf too. Fix taken from upstream OpenBSD without updating
    the rest of stdio.
    
    Change-Id: Ie339a8e9393a36080147aae4d6665118e5d93647

--- libc/stdio/local.h
+++ libc/stdio/local.h
@@ -93,3 +93,5 @@ extern int __sdidinit;
 
 #define FLOCKFILE(fp)   do { if (__isthreaded) flockfile(fp); } while (0)
 #define FUNLOCKFILE(fp) do { if (__isthreaded) funlockfile(fp); } while (0)
+
+#define FLOATING_POINT
