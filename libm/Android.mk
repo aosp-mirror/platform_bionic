770a3495607497071693147f162ac75f39423973
Elliott Hughes <enh@google.com>
2013-10-01 17:57:19 -0700

    Fix libm build for x86_64.
    Change-Id: If89da2d5c3d9a88f78ffd8b260ad0f2fd391c608

--- libm/Android.mk
+++ libm/Android.mk
@@ -217,7 +217,7 @@ libm_common_src_files += fake_long_double.c
 
 # TODO: re-enable i387/e_sqrtf.S for x86, and maybe others.
 
-libm_common_cflags := -DFLT_EVAL_METHOD=0
+libm_common_cflags := -DFLT_EVAL_METHOD=0 -std=c99
 libm_common_includes := $(LOCAL_PATH)/upstream-freebsd/lib/msun/src/
 
 libm_arm_includes := $(LOCAL_PATH)/arm
@@ -226,7 +226,7 @@ libm_arm_src_files := arm/fenv.c
 libm_x86_includes := $(LOCAL_PATH)/i386 $(LOCAL_PATH)/i387
 libm_x86_src_files := i387/fenv.c
 
-libm_x86_64_includes := $(LOCAL_PATH)/amd64 $(LOCAL_PATH)/i387
+libm_x86_64_includes := $(LOCAL_PATH)/amd64
 libm_x86_64_src_files := amd64/fenv.c
 
 libm_mips_cflags := -fno-builtin-rintf -fno-builtin-rint
