54a7494f17f80d6c548a58434bcb5579a26cc125
Elliott Hughes <enh@google.com>
2014-01-03 16:40:37 -0800

    Regenerate the NOTICE files.
    Also standardize the orthography in a few places to reduce near-duplicates
    in the NOTICE files.
    
    Change-Id: I347c75e817be61ec1b9c5b4b96226feedc8d09ab

--- libc/arch-aarch64/bionic/sigsetjmp.S
+++ libc/arch-aarch64/bionic/sigsetjmp.S
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 The Android Open Source Project
- * All rights reserved
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
