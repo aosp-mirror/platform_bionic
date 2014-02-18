2f3ba8844ab8ed6be02485c79c1e974a6513b492
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2014-02-18 10:22:58 +0200

    Add accept4 unit test to bionic.
    Change-Id: I1cce5fb8dab16f03164fdd288f35a814d7c49c6a

--- tests/Android.mk
+++ tests/Android.mk
@@ -81,6 +81,7 @@ libBionicStandardTests_src_files := \
     system_properties_test.cpp \
     time_test.cpp \
     unistd_test.cpp \
+    accept4_test.cpp \
 
 libBionicStandardTests_cflags := \
     $(test_cflags) \
