da030de702b756077fec536ed139e7ea38b51022
Calin Juravle <calin@google.com>
2014-02-20 13:40:36 +0000

    Fixed int fast types for LP64
    Bug: 13110474
    Change-Id: Id5591603ad4ba7e149c0852f4abb25e4e5a5d065

--- libc/include/stdint.h
+++ libc/include/stdint.h
@@ -69,28 +69,34 @@ typedef __intptr_t    intptr_t;
 typedef __uintptr_t   uintptr_t;
 
 typedef int8_t        int_least8_t;
-typedef int8_t        int_fast8_t;
-
 typedef uint8_t       uint_least8_t;
-typedef uint8_t       uint_fast8_t;
 
 typedef int16_t       int_least16_t;
-typedef int32_t       int_fast16_t;
-
 typedef uint16_t      uint_least16_t;
-typedef uint32_t      uint_fast16_t;
 
 typedef int32_t       int_least32_t;
-typedef int32_t       int_fast32_t;
-
 typedef uint32_t      uint_least32_t;
-typedef uint32_t      uint_fast32_t;
 
 typedef int64_t       int_least64_t;
-typedef int64_t       int_fast64_t;
-
 typedef uint64_t      uint_least64_t;
+
+typedef int8_t        int_fast8_t;
+typedef uint8_t       uint_fast8_t;
+
+typedef int64_t       int_fast64_t;
 typedef uint64_t      uint_fast64_t;
+
+#ifdef __LP64__
+typedef int64_t       int_fast16_t;
+typedef uint64_t      uint_fast16_t;
+typedef int64_t       int_fast32_t;
+typedef uint64_t      uint_fast32_t;
+#else
+typedef int32_t       int_fast16_t;
+typedef uint32_t      uint_fast16_t;
+typedef int32_t       int_fast32_t;
+typedef uint32_t      uint_fast32_t;
+#endif
 
 typedef uint64_t      uintmax_t;
 typedef int64_t       intmax_t;
