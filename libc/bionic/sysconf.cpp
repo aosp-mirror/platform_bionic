a0ca209ac0deef086c0e69b59168dd75df2a3f52
Calin Juravle <calin@google.com>
2014-03-10 18:25:36 +0000

    Sysconfig fixes
    Remove _POSIX_THREAD_ATTR_STACKADDR and _POSIX_THREAD_ATTR_STACKSIZE
    since they don't exists. Return -1 for their corresponding values but
    don't set errno.
    
    Bug: 13281069
    Change-Id: Ice29b4dfebe2b474212e40ee726d86782a3064b9

--- libc/bionic/sysconf.cpp
+++ libc/bionic/sysconf.cpp
@@ -317,12 +317,10 @@ int sysconf(int name) {
 #ifdef _POSIX_THREADS
     case _SC_THREADS:             return _POSIX_THREADS;
 #endif
-#ifdef _POSIX_THREAD_ATTR_STACKADDR
-    case _SC_THREAD_ATTR_STACKADDR:   return _POSIX_THREAD_ATTR_STACKADDR;
-#endif
-#ifdef _POSIX_THREAD_ATTR_STACKSIZE
-    case _SC_THREAD_ATTR_STACKSIZE:   return _POSIX_THREAD_ATTR_STACKSIZE;
-#endif
+
+    case _SC_THREAD_ATTR_STACKADDR:   return -1; // Removed in POSIX 2008
+    case _SC_THREAD_ATTR_STACKSIZE:   return -1; // Removed in POSIX 2008
+
 #ifdef _POSIX_THREAD_PRIORITY_SCHEDULING
     case _SC_THREAD_PRIORITY_SCHEDULING:  return _POSIX_THREAD_PRIORITY_SCHEDULING;
 #endif
