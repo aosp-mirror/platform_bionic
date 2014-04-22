8d77bce185d04c531bd9bf34d38e5cbbe644d3a3
Elliott Hughes <enh@google.com>
2014-04-22 13:55:58 -0700

    LP64 shouldn't include the non-standard <time64.h> cruft.
    This patch includes just enough to keep external/chromium_org building
    until they switch 64-bit Android over to using the regular non-Android code.
    
    Change-Id: Iecaf274efa46ae18a42d5e3439c5aa4f909177c1

--- libc/arch-arm/arm.mk
+++ libc/arch-arm/arm.mk
@@ -4,6 +4,7 @@
 libc_common_src_files_arm := \
     bionic/legacy_32_bit_support.cpp \
     bionic/ndk_cruft.cpp \
+    bionic/time64.c \
 
 # These are shared by all the 32-bit targets, but not the 64-bit ones.
 libc_bionic_src_files_arm := \
