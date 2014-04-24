5a92284167ffba6d45210ef6889fa7d255c15d4f
Varvara Rainchik <varvara.rainchik@intel.com>
2014-04-24 15:41:20 +0400

    Add 32-bit Silvermont-optimized string/memory functions.
    Add following functions:
    bcopy, memcpy, memmove, memset, bzero, memcmp, wmemcmp, strlen,
    strcpy, strncpy, stpcpy, stpncpy.
    Create new directories inside arch-x86 to specify architecture: atom,
    silvermont and generic (non atom or silvermont architectures are treated like generic).
    Due to introducing optimized versions of stpcpy and stpncpy,
    c-implementations of these functions are moved from
    common for architectures makefile to arm and mips specific makefiles.
    
    Change-Id: I990f8061c3e9bca1f154119303da9e781c5d086e
    Signed-off-by: Varvara Rainchik <varvara.rainchik@intel.com>

--- libc/arch-mips64/mips64.mk
+++ libc/arch-mips64/mips64.mk
@@ -17,6 +17,8 @@ libc_common_src_files_mips64 := \
     upstream-freebsd/lib/libc/string/wcsrchr.c \
     upstream-freebsd/lib/libc/string/wmemcmp.c \
     upstream-openbsd/lib/libc/string/bcopy.c \
+    upstream-openbsd/lib/libc/string/stpcpy.c \
+    upstream-openbsd/lib/libc/string/stpncpy.c \
     upstream-openbsd/lib/libc/string/strcat.c \
     upstream-openbsd/lib/libc/string/strcmp.c \
     upstream-openbsd/lib/libc/string/strcpy.c \
