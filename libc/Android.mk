15b641a26731a7e42455c3ed22e1e9bdf31ea79c
Elliott Hughes <enh@google.com>
2014-05-14 18:18:55 -0700

    Move libstdc++ into libc.
    The Android build system always links against libstdc++.so anyway. Having
    operator new and operator delete in a separate library means we can't use
    constructors and destructors on heap-allocated objects inside the C library,
    which is quite an unfortunate limitation.
    
    This will be cheaper too; on LP64 we can stop linking against the [now empty]
    libstdc++.so giving the dynamic linker one less library to worry about for
    every process.
    
    There's precedent too --- we already have no libpthread or librt.
    
    For now I'm leaving the include files where they are, and I'm generating a
    dummy libstdc++.so and libstdc++.a. We can come back and clean that up later
    if all goes well.
    
    Bug: 13367666
    Change-Id: I6f3e27ea7c30d03d6394965d0400c9dc87fa83db

--- libc/Android.mk
+++ libc/Android.mk
@@ -112,6 +112,8 @@ libc_bionic_src_files := \
     bionic/clone.cpp \
     bionic/cmsg_nxthdr.cpp \
     bionic/connect.cpp \
+    bionic/__cxa_guard.cpp \
+    bionic/__cxa_pure_virtual.cpp \
     bionic/dirent.cpp \
     bionic/dup2.cpp \
     bionic/epoll_create.cpp \
@@ -144,6 +146,7 @@ libc_bionic_src_files := \
     bionic/mknod.cpp \
     bionic/mntent.cpp \
     bionic/NetdClientDispatch.cpp \
+    bionic/new.cpp \
     bionic/open.cpp \
     bionic/pause.cpp \
     bionic/pipe.cpp \
@@ -213,6 +216,7 @@ libc_bionic_src_files := \
     bionic/termios.cpp \
     bionic/thread_atexit.cpp \
     bionic/tmpfile.cpp \
+    bionic/typeinfo.cpp \
     bionic/umount.cpp \
     bionic/unlink.cpp \
     bionic/utimes.cpp \
---
+++ libstdc++/src/libstdc++.cpp
@@ -0,0 +1,1 @@
+extern "C" void __this_library_is_now_part_of_libc() {}
