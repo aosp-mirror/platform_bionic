c87f65d2cd0690d81665f8b241c1d763f72b6f80
Dmitriy Ivanov <dimitry@google.com>
2014-05-19 15:06:58 -0700

    Add RTLD_NODELETE flag support
    Bug: 18186310
    Bug: https://code.google.com/p/android/issues/detail?id=64069
    
    (cherry picked from commit 1b20dafdbe65e43b9f4c95057e8482380833ea91)
    
    Change-Id: Ic02eec22a7c322ece65eb40730a3404f611526b1

---
+++ tests/libs/dlopen_nodelete_2.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t dlopen_nodelete_2_taxicab_number = 1729;
+static bool* unload_flag_ptr = nullptr;
+
+extern "C" void dlopen_nodelete_2_set_unload_flag_ptr(bool* ptr) {
+  unload_flag_ptr = ptr;
+}
+
+static void __attribute__((destructor)) unload_guard() {
+  if (unload_flag_ptr != nullptr) {
+    *unload_flag_ptr = true;
+  }
+}
