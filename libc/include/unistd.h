b27a840f4b520bfa095db99b0a2e5205634b0003
Elliott Hughes <enh@google.com>
2014-06-10 20:47:49 -0700

    Add __pure2 to a few more functions, most notably gettid and pthread_self.
    Change-Id: I7eee9f26f45130038af09d8285782b07f70a996f

--- libc/include/unistd.h
+++ libc/include/unistd.h
@@ -54,7 +54,7 @@ extern __noreturn void _exit(int);
 extern pid_t  fork(void);
 extern pid_t  vfork(void);
 extern pid_t  getpid(void);
-extern pid_t  gettid(void);
+extern pid_t  gettid(void) __pure2;
 extern pid_t  getpgid(pid_t);
 extern int    setpgid(pid_t, pid_t);
 extern pid_t  getppid(void);
