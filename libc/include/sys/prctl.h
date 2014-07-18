de24f1ac00924b9a523d06784867bc8c9312335d
Elliott Hughes <enh@google.com>
2014-07-18 15:55:41 -0700

    Clean up some misinformation around prctl.
    prctl shouldn't be in <unistd.h>.
    
    (cherry picked from commit 9c07aee83b4ebbf2dba8e23d6896683187b9724e)
    
    Change-Id: I70cda886fbf4d58d82dc70adaa981506ebff9949

--- libc/include/sys/prctl.h
+++ libc/include/sys/prctl.h
@@ -25,20 +25,16 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
 #ifndef _SYS_PRCTL_H
 #define _SYS_PRCTL_H
 
-#include <linux/prctl.h>
 #include <sys/cdefs.h>
+
+#include <linux/prctl.h>
 
 __BEGIN_DECLS
 
-/* IMPORTANT NOTE: This function is declared as taking a variable number
- *                 of arguments to match the GLibc definition. However
- *                 its declaration inside SYSCALLS.TXT *must* make it
- *                 take 6 arguments to ensure consistency with the kernel
- *                 implementation.
- */
 extern int prctl(int option, ...);
 
 __END_DECLS
