62c69c25d29c42359c4383319d0080c204789a8d
Ben Murdoch <benm@google.com>
2014-08-06 16:17:04 +0100

    fix the build
    Change-Id: I8a3b26014ced1ce95b9a0f675099c3fdfbe041f3

--- libc/Android.mk
+++ libc/Android.mk
@@ -226,7 +226,6 @@ libc_bionic_src_files := \
     bionic/unlink.cpp \
     bionic/utimes.cpp \
     bionic/vdso.cpp \
-    bionic/vfork.cpp \
     bionic/wait.cpp \
     bionic/wchar.cpp \
     bionic/wctype.cpp \
