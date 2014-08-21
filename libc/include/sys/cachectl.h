47793d6a29f63869e7dbc583576cb8bf351cd504
Dan Albert <danalbert@google.com>
2014-08-21 12:13:24 -0700

    Remove _flush_cache(2) for mips64.
    Also remove declaration.
    
    The only user is compiler-rt, and they can replace that call with one to
    syscall(2). compiler-rt doesn't currently build on mips64 anyway.
    
    Bug: 11156955
    Change-Id: Ieae0ba49c8e7aa50253401fc1d7c2d17bc867d39

--- libc/include/sys/cachectl.h
+++ libc/include/sys/cachectl.h
@@ -31,6 +31,5 @@
 #ifdef __mips__
 #include <asm/cachectl.h>
 extern int __cachectl (void *addr, __const int nbytes, __const int op);
-extern int _flush_cache (char *addr, __const int nbytes, __const int op);
 #endif
 #endif /* sys/cachectl.h */
