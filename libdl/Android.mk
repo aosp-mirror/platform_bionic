3ec67de0513c1d7e5f41f7f17e8251122a0cc9d2
Dan Albert <danalbert@google.com>
2014-09-16 10:10:25 -0700

    Exclude libstdc++ from linked libraries.
    Also explicitly add libstdc++ includes for necessary targets.
    
    Change-Id: If712ba0ae7908d8147a69e29da5c453a183d6540

--- libdl/Android.mk
+++ libdl/Android.mk
@@ -23,6 +23,7 @@ LOCAL_LDFLAGS_x86_64 := $(LOCAL_LDFLAGS_x86)
 
 LOCAL_SRC_FILES:= libdl.c
 LOCAL_CFLAGS := -Wall -Wextra -Wunused -Werror
+LOCAL_CXX_STL := none
 
 LOCAL_MODULE := libdl
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
