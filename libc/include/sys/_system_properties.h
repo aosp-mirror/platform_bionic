71d220c1de1372e20c8bbec4ccf387991a3bb549
Daniel Rosenberg <drosen@google.com>
2014-11-10 16:59:57 -0800

    bionic: libc: Added path to vendor build properties file.
    Change-Id: Ia9fc257a82fdd592751cbcde3fcf453a49856f62
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- libc/include/sys/_system_properties.h
+++ libc/include/sys/_system_properties.h
@@ -81,6 +81,7 @@ struct prop_msg
 #define PROP_PATH_RAMDISK_DEFAULT  "/default.prop"
 #define PROP_PATH_SYSTEM_BUILD     "/system/build.prop"
 #define PROP_PATH_SYSTEM_DEFAULT   "/system/default.prop"
+#define PROP_PATH_VENDOR_BUILD     "/vendor/build.prop"
 #define PROP_PATH_LOCAL_OVERRIDE   "/data/local.prop"
 #define PROP_PATH_FACTORY          "/factory/factory.prop"
 
