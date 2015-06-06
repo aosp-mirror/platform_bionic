4900f1c4cd41f57c6d453ecd84fd82b8d7d9e8fa
Ying Wang <wangying@google.com>
2015-06-06 11:55:16 -0700

    Fix Mac checkbuild.
    Bug: 21669400
    Change-Id: I54691661605a2e2c1871781964aed343840b8560
    
    (cherry-pick from commit 33c2d441b8f816722bed984c6e12b82b888c3ca2)

--- tools/relocation_packer/Android.mk
+++ tools/relocation_packer/Android.mk
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+ifeq ($(HOST_OS),linux)
 common_cppflags := -Wall -Wextra -Wunused -Werror -Wold-style-cast
 
 LOCAL_PATH := $(call my-dir)
@@ -101,3 +102,5 @@ $(eval $(call copy-test-library,elf_file_unittest_relocs_x64.so))
 $(eval $(call copy-test-library,elf_file_unittest_relocs_x64_packed.so))
 $(eval $(call copy-test-library,elf_file_unittest_relocs_mips32.so))
 $(eval $(call copy-test-library,elf_file_unittest_relocs_mips32_packed.so))
+
+endif
