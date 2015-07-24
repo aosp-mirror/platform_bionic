5930772286de72a6cec1a9c7513355efe238e7ef
Christopher Ferris <cferris@google.com>
2015-07-23 20:27:42 -0700

    Add optimized cortex-a7/cortex-a53 memset/memcpy.
    Add an optimized memset that is ~20% faster for cortex-a7 and
    cortex-a53.
    
    Add a 32 bit optimized cortex-a53 memcpy that is about ~20% faster
    on cached data.
    
    Fix the cortex-a15 __str{cat,cpy}_chk.S, memcpy_base.S to remove
    the phony functions, since they aren't needed any more. Then add
    a direct include of these for cortex-a53.
    
    Verified the new functions by stepping through all of the major
    paths and verifying the backtrace is still correct.
    
    Bug: 22696180
    Change-Id: Iec92a3f82d51243cca76c9aff9f35d920ff865ae

--- libc/arch-arm/cortex-a7/cortex-a7.mk
+++ libc/arch-arm/cortex-a7/cortex-a7.mk
@@ -1,1 +1,18 @@
-include bionic/libc/arch-arm/cortex-a15/cortex-a15.mk
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a7/bionic/memset.S \
+
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a15/bionic/memcpy.S \
+    arch-arm/cortex-a15/bionic/stpcpy.S \
+    arch-arm/cortex-a15/bionic/strcat.S \
+    arch-arm/cortex-a15/bionic/__strcat_chk.S \
+    arch-arm/cortex-a15/bionic/strcmp.S \
+    arch-arm/cortex-a15/bionic/strcpy.S \
+    arch-arm/cortex-a15/bionic/__strcpy_chk.S \
+    arch-arm/cortex-a15/bionic/strlen.S \
+
+libc_bionic_src_files_arm += \
+    arch-arm/generic/bionic/memcmp.S \
+
+libc_bionic_src_files_arm += \
+    arch-arm/denver/bionic/memmove.S \
