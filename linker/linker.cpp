f74b041cce75091b922151d4fa57e63c8a6caf4b
Hung-ying Tyan <tyanh@google.com>
2015-11-12 11:48:19 +0800

    Add /odm/lib to shared lib search path
    ODM's shared libs should live in /odm/lib on the ODM partition.
    
    BUG: 25654402
    Change-Id: I3f5c26f208af87ff2817e490e51d0b4f5b8ec98e

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -125,9 +125,11 @@ static soinfo* somain; // main process, always the one after libdl_info
 
 static const char* const kDefaultLdPaths[] = {
 #if defined(__LP64__)
+  "/odm/lib64",
   "/vendor/lib64",
   "/system/lib64",
 #else
+  "/odm/lib",
   "/vendor/lib",
   "/system/lib",
 #endif
@@ -136,11 +138,15 @@ static const char* const kDefaultLdPaths[] = {
 
 static const char* const kAsanDefaultLdPaths[] = {
 #if defined(__LP64__)
+  "/data/odm/lib64",
+  "/odm/lib64",
   "/data/vendor/lib64",
   "/vendor/lib64",
   "/data/lib64",
   "/system/lib64",
 #else
+  "/data/odm/lib",
+  "/odm/lib",
   "/data/vendor/lib",
   "/vendor/lib",
   "/data/lib",
