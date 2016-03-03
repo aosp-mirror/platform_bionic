bed110af26f947057fd7940ba383b6f562d2df97
Elliott Hughes <enh@google.com>
2016-03-03 10:41:42 -0800

    Clean up bcopy cruft.
    Also remove some generic x86 assembler that's never built.
    
    Change-Id: Ic126de2eee646b5ba2b6ee549679ba90d67a48bb

--- libc/arch-x86/atom/atom.mk
+++ libc/arch-x86/atom/atom.mk
@@ -1,7 +1,6 @@
 libc_bionic_src_files_x86 += \
     arch-x86/atom/string/sse2-memset-atom.S \
     arch-x86/atom/string/sse2-strlen-atom.S \
-    arch-x86/atom/string/ssse3-bcopy-atom.S \
     arch-x86/atom/string/ssse3-memcmp-atom.S \
     arch-x86/atom/string/ssse3-memcpy-atom.S \
     arch-x86/atom/string/ssse3-memmove-atom.S \
@@ -13,7 +12,6 @@ libc_bionic_src_files_exclude_x86 += \
     arch-x86/generic/string/memcmp.S \
 
 libc_bionic_src_files_exclude_x86 += \
-    arch-x86/silvermont/string/sse2-bcopy-slm.S \
     arch-x86/silvermont/string/sse2-memcpy-slm.S \
     arch-x86/silvermont/string/sse2-memmove-slm.S \
     arch-x86/silvermont/string/sse2-memset-slm.S \
