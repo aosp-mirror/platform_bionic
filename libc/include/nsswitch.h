c194fd30c6fa3537d918939dd0a762786f21d764
Josh Gao <jmgao@google.com>
2016-04-25 17:04:10 -0700

    Delete declarations which don't exist.
    Bug: http://b/28178111
    Change-Id: I72d5d015d7927a8df24b3973bf8a052053a067fb

--- libc/include/nsswitch.h
+++ libc/include/nsswitch.h
@@ -144,19 +144,6 @@ typedef struct {
 
 
 /*
- * Default sourcelists (if nsswitch.conf is missing, corrupt,
- * or the requested database doesn't have an entry)
- */
-extern const ns_src __nsdefaultsrc[];
-extern const ns_src __nsdefaultcompat[];
-extern const ns_src __nsdefaultcompat_forceall[];
-extern const ns_src __nsdefaultfiles[];
-extern const ns_src __nsdefaultfiles_forceall[];
-extern const ns_src __nsdefaultnis[];
-extern const ns_src __nsdefaultnis_forceall[];
-
-
-/*
  * ns_mtab - `nsswitch method table'
  * An nsswitch module provides a mapping from (database name, method name)
  * tuples to the nss_method and associated callback data.  Effectively,
