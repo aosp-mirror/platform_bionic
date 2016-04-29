5a3d5cac642f9e58b2c510eab23789fb4fc3a753
Josh Gao <jmgao@google.com>
2016-04-29 12:15:18 -0700

    Add __INTRODUCED_IN_{32,64}.
    Some symbols appeared at different times between 32 and 64 bit. Add a
    macro to represent this.
    
    Bug: http://b/28178111
    Change-Id: I0aa46d9da3c7301b60df0416bce252f0f37b7b36

--- tests/complex_test.cpp
+++ tests/complex_test.cpp
@@ -21,6 +21,8 @@
 #include <sys/cdefs.h>
 #if !defined(__INTRODUCED_IN)
 #define __INTRODUCED_IN(x)
+#define __INTRODUCED_IN_32(x)
+#define __INTRODUCED_IN_64(x)
 #endif
 
 // libc++ actively gets in the way of including <complex.h> from C++, so we
