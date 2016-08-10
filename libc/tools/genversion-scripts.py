5ffed9b8560636e1a00b4383ec1d18489e44821c
Elliott Hughes <enh@google.com>
2016-08-10 14:06:14 -0700

    Move brillo closer to Android.
    Hiding our legacy cruft seemed like a good idea, but in practice it will only
    mean worse interoperability.
    
    Plus we got it wrong, as the recent `putw` example showed.
    
    Change-Id: I167c7168eff133889028089c22a7a0dfb8d6d0cf

--- libc/tools/genversion-scripts.py
+++ libc/tools/genversion-scripts.py
@@ -37,8 +37,6 @@ def has_arch_tags(tags):
   for arch in all_arches:
     if arch in tags:
       return True
-  if 'nobrillo' in tags:
-    return True
   return False
 
 
@@ -49,28 +47,21 @@ class VersionScriptGenerator(object):
       basename = os.path.basename(script)
       dirname = os.path.dirname(script)
       for arch in all_arches:
-        for brillo in [False, True]:
-          has_nobrillo = False
-          name = basename.split(".")[0] + "." + arch + (".brillo" if brillo else "") + ".map"
-          tmp_path = os.path.join(bionic_temp, name)
-          dest_path = os.path.join(dirname, name)
-          with open(tmp_path, "w") as fout:
-            with open(script, "r") as fin:
-              fout.write("# %s\n" % warning)
-              for line in fin:
-                index = line.find("#")
-                if index != -1:
-                  tags = line[index+1:].split()
-                  if arch not in tags and has_arch_tags(tags):
-                    continue
-                  if brillo and "nobrillo" in tags:
-                    has_nobrillo = True
-                    continue
-                fout.write(line)
-          if not brillo or has_nobrillo:
-            shutil.copyfile(tmp_path, dest_path)
+        name = basename.split(".")[0] + "." + arch + ".map"
+        tmp_path = os.path.join(bionic_temp, name)
+        dest_path = os.path.join(dirname, name)
+        with open(tmp_path, "w") as fout:
+          with open(script, "r") as fin:
+            fout.write("# %s\n" % warning)
+            for line in fin:
+              index = line.find("#")
+              if index != -1:
+                tags = line[index+1:].split()
+                if arch not in tags and has_arch_tags(tags):
+                  continue
+              fout.write(line)
+        shutil.copyfile(tmp_path, dest_path)
 
 
 generator = VersionScriptGenerator()
 generator.run()
-
