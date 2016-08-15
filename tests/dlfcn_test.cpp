46230445172d3cd72c38102d57f5a1b725c80367
Dimitry Ivanov <dimitry@google.com>
2016-08-15 13:40:53 -0700

    linker: add test for zero shdr offset
    Bug: http://b/30795430
    Change-Id: I03853257c4b632ad9daaf1e90f9620678397cff0
    Test: bionic-unit-tests --gtest_filter=dlfcn.dlopen_invalid*

--- tests/dlfcn_test.cpp
+++ tests/dlfcn_test.cpp
@@ -1194,4 +1194,12 @@ TEST(dlfcn, dlopen_invalid_empty_shdr_table) {
   ASSERT_STREQ(expected_dlerror.c_str(), dlerror());
 }
 
+TEST(dlfcn, dlopen_invalid_zero_shdr_table_offset) {
+  std::string libpath = std::string(getenv("ANDROID_DATA")) + PREBUILT_ELF_PATH + "/libtest_invalid-zero_shdr_table_offset.so";
+  void* handle = dlopen(libpath.c_str(), RTLD_NOW);
+  ASSERT_TRUE(handle == nullptr);
+  std::string expected_dlerror = std::string("dlopen failed: \"") + libpath + "\" has invalid shdr offset/size: 0/";
+  ASSERT_SUBSTR(expected_dlerror.c_str(), dlerror());
+}
+
 #endif
