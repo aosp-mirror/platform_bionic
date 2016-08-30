spsforks/android-bionic-libc

Remove $(module)_install_to_out_data flag from test makefile
Use non-empty $(module)_install_to_out_data_dir as indication
of custom target dir for a library.

Bug: http://b/22182538
Test: build and run bionic-unit-tests --gtest_filter=dl*:Dl*
Change-Id: Ibfc87f45084a6fe2f487ca1b17a7625a8d8ec707

tests/libs/Android.build.dt_runpath.mk
@@ -76,7 +76,7 @@ libtest_dt_runpath_d_zip_src_files := \
 
 libtest_dt_runpath_d_zip_shared_libraries := libtest_dt_runpath_b libtest_dt_runpath_c
 libtest_dt_runpath_d_zip_ldflags := -Wl,--rpath,\$${ORIGIN}/dt_runpath_b_c_x -Wl,--enable-new-dtags
-libtest_dt_runpath_d_zip_install_to_out_data := true
+libtest_dt_runpath_d_zip_install_to_out_data_dir := libtest_dt_runpath_d_zip
 module := libtest_dt_runpath_d_zip
 module_tag := optional
 build_type := target

tests/libs/Android.mk
@@ -75,7 +75,7 @@ libdlext_test_fd_src_files := \
 
 libdlext_test_fd_shared_libraries := libtest_simple
 
-libdlext_test_fd_install_to_out_data := true
+libdlext_test_fd_install_to_out_data_dir := libdlext_test_fd
 module := libdlext_test_fd
 module_tag := optional
 build_type := target
@@ -91,7 +91,7 @@ libdlext_test_zip_src_files := \
 
 libdlext_test_zip_shared_libraries := libatest_simple_zip
 
-libdlext_test_zip_install_to_out_data := true
+libdlext_test_zip_install_to_out_data_dir := libdlext_test_zip
 module := libdlext_test_zip
 module_tag := optional
 build_type := target
@@ -101,7 +101,7 @@ include $(TEST_PATH)/Android.build.mk
 libatest_simple_zip_src_files := \
     dlopen_testlib_simple.cpp
 
-libatest_simple_zip_install_to_out_data := true
+libatest_simple_zip_install_to_out_data_dir := libatest_simple_zip
 module := libatest_simple_zip
 module_tag := optional
 build_type := target

aosp-mirror/platform_bionic

tests/libs/Android.build.dt_runpath.mk
@@ -71,13 +71,15 @@ module := libtest_dt_runpath_d
 include $(LOCAL_PATH)/Android.build.testlib.mk
 
 # D version for open-from-zip test with runpath
+module := libtest_dt_runpath_d_zip
+
 libtest_dt_runpath_d_zip_src_files := \
     dlopen_b.cpp
 
 libtest_dt_runpath_d_zip_shared_libraries := libtest_dt_runpath_b libtest_dt_runpath_c
 libtest_dt_runpath_d_zip_ldflags := -Wl,--rpath,\$${ORIGIN}/dt_runpath_b_c_x -Wl,--enable-new-dtags
-libtest_dt_runpath_d_zip_install_to_out_data := true
-module := libtest_dt_runpath_d_zip
+libtest_dt_runpath_d_zip_install_to_out_data_dir := $(module)
+
 module_tag := optional
 build_type := target
 build_target := SHARED_LIBRARY

tests/libs/Android.mk
@@ -70,13 +70,14 @@ $(HOST_OUT)/lib/libdlext_test.so: PRIVATE_POST_INSTALL_CMD = \
 # -----------------------------------------------------------------------------
 # Library used by dlext tests - different name non-default location
 # -----------------------------------------------------------------------------
+module := libdlext_test_fd
+
 libdlext_test_fd_src_files := \
     dlext_test_library.cpp \
 
 libdlext_test_fd_shared_libraries := libtest_simple
 
-libdlext_test_fd_install_to_out_data := true
-module := libdlext_test_fd
+libdlext_test_fd_install_to_out_data_dir := $(module)
 module_tag := optional
 build_type := target
 build_target := SHARED_LIBRARY
@@ -86,23 +87,25 @@ include $(TEST_PATH)/Android.build.mk
 # -----------------------------------------------------------------------------
 # Libraries used by dlext tests for open from a zip-file
 # -----------------------------------------------------------------------------
+module := libdlext_test_zip
+
 libdlext_test_zip_src_files := \
     dlext_test_library.cpp \
 
 libdlext_test_zip_shared_libraries := libatest_simple_zip
 
-libdlext_test_zip_install_to_out_data := true
-module := libdlext_test_zip
+libdlext_test_zip_install_to_out_data_dir := $(module)
 module_tag := optional
 build_type := target
 build_target := SHARED_LIBRARY
 include $(TEST_PATH)/Android.build.mk
 
+module := libatest_simple_zip
+
 libatest_simple_zip_src_files := \
     dlopen_testlib_simple.cpp
 
-libatest_simple_zip_install_to_out_data := true
-module := libatest_simple_zip
+libatest_simple_zip_install_to_out_data_dir := $(module)
 module_tag := optional
 build_type := target
 build_target := SHARED_LIBRARY