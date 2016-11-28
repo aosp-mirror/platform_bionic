8402b42312921ab0f819e324f468fd478e8c3708
Christopher Ferris <cferris@google.com>
2016-11-28 11:09:49 -0800

    Add cortex-a7 specific routines.
    Test: Changed angler target to use cortex-a7 and I compiled.
    Test: Booted this version on angler and ran bionic-unit-tests.
    
    Bug: 31917556
    
    Change-Id: Ice7f6ea38a2569582161a8e659d7877918c1a45a
    (cherry picked from commit ecebb49ac636b90b3db48b7375c26c07c95fd104)

--- libc/arch-arm/cortex-a7/cortex-a7.mk
+++ libc/arch-arm/cortex-a7/cortex-a7.mk
@@ -13,16 +13,16 @@ libc_bionic_src_files_exclude_arm += \
     bionic/__strcpy_chk.cpp \
 
 libc_bionic_src_files_arm += \
+    arch-arm/cortex-a7/bionic/memcpy.S \
     arch-arm/cortex-a7/bionic/memset.S \
+    arch-arm/cortex-a7/bionic/__strcat_chk.S \
+    arch-arm/cortex-a7/bionic/__strcpy_chk.S \
 
 libc_bionic_src_files_arm += \
-    arch-arm/cortex-a15/bionic/memcpy.S \
     arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/cortex-a15/bionic/strcat.S \
-    arch-arm/cortex-a15/bionic/__strcat_chk.S \
     arch-arm/cortex-a15/bionic/strcmp.S \
     arch-arm/cortex-a15/bionic/strcpy.S \
-    arch-arm/cortex-a15/bionic/__strcpy_chk.S \
     arch-arm/cortex-a15/bionic/strlen.S \
 
 libc_bionic_src_files_arm += \
