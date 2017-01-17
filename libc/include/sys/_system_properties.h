dc94cf5cd8fb943cb5bcbaccc54c401de7418de4
Jaekyun Seok <jaekyun@google.com>
2017-01-17 14:44:31 +0900

    Add props files for each partition
    The following files will be added.
    - /vendor/default.prop for default prop overrides from vendor
     partition.
    - /odm/default.prop for default prop overrides from odm partition.
    - /odm/build.prop for build prop overrides from odm partition.
    
    Test: tested default/build prop files with enabling early mount, but
    didn't test files of odm partition because odm partition doesn't
    exist now.
    Bug: 34116668
    Change-Id: Ia99895a0afcdc522463d95b2f3e2841dcf2e3516

--- libc/include/sys/_system_properties.h
+++ libc/include/sys/_system_properties.h
@@ -82,8 +82,11 @@ struct prop_msg
 */
 
 #define PROP_PATH_RAMDISK_DEFAULT  "/default.prop"
+#define PROP_PATH_VENDOR_DEFAULT   "/vendor/default.prop"
+#define PROP_PATH_ODM_DEFAULT      "/odm/default.prop"
 #define PROP_PATH_SYSTEM_BUILD     "/system/build.prop"
 #define PROP_PATH_VENDOR_BUILD     "/vendor/build.prop"
+#define PROP_PATH_ODM_BUILD        "/odm/build.prop"
 #define PROP_PATH_LOCAL_OVERRIDE   "/data/local.prop"
 #define PROP_PATH_FACTORY          "/factory/factory.prop"
 
