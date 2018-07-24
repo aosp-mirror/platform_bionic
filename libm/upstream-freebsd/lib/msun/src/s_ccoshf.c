ab528076854c7bc2bb5ad30ead5aadf630fb7de8
Elliott Hughes <enh@google.com>
2018-07-24 00:01:52 +0000

    Update to FreeBSD libm r336665.
    This reverts commit 253a8306316cedfd6fd3e3a169fbffe4cac04035 and moves
    us forward to a revision that contains fixes for the problem with the
    previous attempt.
    
    This also makes sincos(3)/sincosf(3)/sincosl(3) available to `_BSD_SOURCE`
    as well as `_GNU_SOURCE`.
    
    The new FreeBSD libm code requires the FreeBSD `__CONCAT` macro, and all
    our existing callers are FreeBSD too, so update that.
    
    There's also an assumption that <complex.h> drags in <math.h> which isn't
    true for us, so work around that with `-include` in the makefile. This
    then causes clang to recognize a bug -- returning from a void function --
    in our fake (LP32) sincosl(3), so fix that too.
    
    Bug: http://b/111710419
    Change-Id: I84703ad844f8afde6ec6b11604ab3c096ccb62c3
    Test: ran tests

--- libm/upstream-freebsd/lib/msun/src/s_ccoshf.c
+++ libm/upstream-freebsd/lib/msun/src/s_ccoshf.c
@@ -31,7 +31,7 @@
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/lib/msun/src/s_ccoshf.c 326219 2017-11-26 02:00:33Z pfg $");
+__FBSDID("$FreeBSD: head/lib/msun/src/s_ccoshf.c 336362 2018-07-17 07:42:14Z bde $");
 
 #include <complex.h>
 #include <math.h>
@@ -92,7 +92,8 @@ ccoshf(float complex z)
 		return (CMPLXF(INFINITY * cosf(y), x * sinf(y)));
 	}
 
-	return (CMPLXF((x * x) * (y - y), (x + x) * (y - y)));
+	return (CMPLXF(((long double)x * x) * (y - y),
+	    ((long double)x + x) * (y - y)));
 }
 
 float complex
