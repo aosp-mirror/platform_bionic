a6c71a09670ca636cca5cfea9d74b03a951e2b5e
Elliott Hughes <enh@google.com>
2018-11-12 16:01:37 -0800

    Move API levels from `uint32_t` to `int`.
    Bug: N/A
    Test: builds
    Change-Id: I9c414e30e3c4fe2a4e16a2fe4ce18eae85fe4844

--- linker/linker_soinfo.h
+++ linker/linker_soinfo.h
@@ -276,7 +276,7 @@ struct soinfo {
   ElfW(Addr) get_verdef_ptr() const;
   size_t get_verdef_cnt() const;
 
-  uint32_t get_target_sdk_version() const;
+  int get_target_sdk_version() const;
 
   void set_dt_runpath(const char *);
   const std::vector<std::string>& get_dt_runpath() const;
@@ -353,7 +353,7 @@ struct soinfo {
   ElfW(Addr) verneed_ptr_;
   size_t verneed_cnt_;
 
-  uint32_t target_sdk_version_;
+  int target_sdk_version_;
 
   // version >= 3
   std::vector<std::string> dt_runpath_;
