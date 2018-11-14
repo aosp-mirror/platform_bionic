734beec3d48a7ea5cfe3bd0815676181ea698cc7
Peter Collingbourne <pcc@google.com>
2018-11-14 12:41:41 -0800

    Allocate a small guard region around the shadow call stack.
    This lets us do two things:
    
    1) Make setjmp and longjmp compatible with shadow call stack.
       To avoid leaking the shadow call stack address into memory, only the
       lower log2(SCS_SIZE) bits of x18 are stored to jmp_buf. This requires
       allocating an additional guard page so that we're guaranteed to be
       able to allocate a sufficiently aligned SCS.
    
    2) SCS overflow detection. Overflows now result in a SIGSEGV instead
       of corrupting the allocation that comes after it.
    
    Change-Id: I04d6634f96162bf625684672a87fba8b402b7fd1
    Test: bionic-unit-tests

--- libc/private/bionic_constants.h
+++ libc/private/bionic_constants.h
@@ -19,4 +19,14 @@
 
 #define NS_PER_S 1000000000
 
+// Size of the shadow call stack. This must be a power of 2.
+#define SCS_SIZE (8 * 1024)
+
+// The shadow call stack is allocated at an aligned address within a guard region of this size. The
+// guard region must be large enough that we can allocate an SCS_SIZE-aligned SCS while ensuring
+// that there is at least one guard page after the SCS so that a stack overflow results in a SIGSEGV
+// instead of corrupting the allocation that comes after it.
+// TODO(b/118642754): Use a larger guard region.
+#define SCS_GUARD_REGION_SIZE (SCS_SIZE * 2)
+
 #endif // _BIONIC_CONSTANTS_H_
