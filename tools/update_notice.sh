584bc626b65edc09ca8a23426403e736a67599e9
Elliott Hughes <enh@google.com>
2019-04-19 14:18:07 -0700

    Move libdl and linker to static NOTICE files.
    The libstdc++ directory has no copyright headers, so it was a no-op
    anyway.
    
    The interesting part will be switching libc and libm over to genrules...
    
    Test: N/A
    Change-Id: Iec92562af40c451fdcb4a7468984878ec5dba2ce

--- tools/update_notice.sh
+++ tools/update_notice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
-./libc/tools/generate-NOTICE.py libc libdl libm linker libstdc++ > libc/NOTICE
+./libc/tools/generate-NOTICE.py libc libm > libc/NOTICE
 
 git diff --exit-code HEAD libc/NOTICE
 exit $?
