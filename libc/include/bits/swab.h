2dbea434d103567e911f068f73170d7808e071da
Dan Albert <danalbert@google.com>
2019-06-14 14:19:22 -0700

    Make swab(3) inline prior to API 28.
    Test: make checkbuild
    Bug: https://stackoverflow.com/q/54392471/632035
    Change-Id: Iaeb55231ea2395d7e1e31be63034f5db19025060

--- libc/include/bits/swab.h
+++ libc/include/bits/swab.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,7 +26,28 @@
  * SUCH DAMAGE.
  */
 
-#include <unistd.h>
+#pragma once
 
-#define __BIONIC_SWAB_INLINE /* Out of line. */
-#include <bits/swab.h>
+#include <stdint.h>
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+#if !defined(__BIONIC_SWAB_INLINE)
+#define __BIONIC_SWAB_INLINE static __inline
+#endif
+
+__BEGIN_DECLS
+
+__BIONIC_SWAB_INLINE void swab(const void* __void_src, void* __void_dst, ssize_t __byte_count) {
+  const uint8_t* __src = __BIONIC_CAST(static_cast, const uint8_t*, __void_src);
+  uint8_t* __dst = __BIONIC_CAST(static_cast, uint8_t*, __void_dst);
+  while (__byte_count > 1) {
+    uint8_t x = *__src++;
+    uint8_t y = *__src++;
+    *__dst++ = y;
+    *__dst++ = x;
+    __byte_count -= 2;
+  }
+}
+
+__END_DECLS
