b8a95e21865efd16bf821333d34e005b8f0a8d3a
Christopher Ferris <cferris@google.com>
2019-10-02 18:29:20 -0700

    Update to kernel headers v5.3.2.
    Test: Builds and run unit tests on taimen/cuttlefish.
    Change-Id: I6ebd8f179d159ac974555e8edca588083e8081b3

--- libc/kernel/uapi/linux/socket.h
+++ libc/kernel/uapi/linux/socket.h
@@ -19,10 +19,14 @@
 #ifndef _UAPI_LINUX_SOCKET_H
 #define _UAPI_LINUX_SOCKET_H
 #define _K_SS_MAXSIZE 128
-#define _K_SS_ALIGNSIZE (__alignof__(struct sockaddr *))
 typedef unsigned short __kernel_sa_family_t;
 struct sockaddr_storage {
-  __kernel_sa_family_t ss_family;
-  char __data[_K_SS_MAXSIZE - sizeof(unsigned short)];
-} __attribute__((aligned(_K_SS_ALIGNSIZE)));
+  union {
+    struct {
+      __kernel_sa_family_t ss_family;
+      char __data[_K_SS_MAXSIZE - sizeof(unsigned short)];
+    };
+    void * __align;
+  };
+};
 #endif
