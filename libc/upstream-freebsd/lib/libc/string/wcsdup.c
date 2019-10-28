ccea6934fe79a54b30d5d95721b466a9c024dd63
Elliott Hughes <enh@google.com>
2019-10-28 14:23:15 -0700

    Take a bunch of trivial SPDX changes from upstream FreeBSD.
    Test: treehugger
    Change-Id: I2242d47e5ee4e7aaa637e2e2933e0f88b1d05e77

--- libc/upstream-freebsd/lib/libc/string/wcsdup.c
+++ libc/upstream-freebsd/lib/libc/string/wcsdup.c
@@ -1,4 +1,6 @@
 /*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
  * Copyright (c) 2005 Tim J. Robbins.
  * All rights reserved.
  *
@@ -25,7 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
+__FBSDID("$FreeBSD: head/lib/libc/string/wcsdup.c 326193 2017-11-25 17:12:48Z pfg $");
 
 #include <stdlib.h>
 #include <wchar.h>
