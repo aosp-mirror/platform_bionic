f4e3d42f27adcec3342b7c5928842071b991f5a7
Peter Collingbourne <pcc@google.com>
2020-01-10 10:40:11 -0800

    Delete definitions of __aeabi_unwind_cpp_pr0.
    These are no longer necessary now that LLVM no longer emits references to
    this symbol on Android.
    
    Bug: 144430859
    Merged-In: I6c43338f755ce5a79e2df36bd3f2006a748fab27
    (cherry picked from commit 27c1e220361b0b3ab0b8fae35e8767935e8680ec)
    Change-Id: I6c43338f755ce5a79e2df36bd3f2006a748fab27

--- libdl/libdl_android.cpp
+++ libdl/libdl_android.cpp
@@ -115,16 +115,4 @@ struct android_namespace_t* android_get_exported_namespace(const char* name) {
   return __loader_android_get_exported_namespace(name);
 }
 
-#if defined(__arm__)
-// An arm32 unwinding table has an R_ARM_NONE relocation to
-// __aeabi_unwind_cpp_pr0. This shared library will never invoke the unwinder,
-// so it doesn't actually need the routine. Define a dummy version here,
-// because the real version calls libc functions (e.g. memcpy, abort), which
-// would create a dependency cycle with libc.so.
-__attribute__((visibility("hidden")))
-void __aeabi_unwind_cpp_pr0() {
-  __builtin_trap();
-}
-#endif
-
 } // extern "C"
