7a6b65178a232172fee6fdec147022044b14f9a7
Dan Albert <danalbert@google.com>
2020-03-09 16:48:18 -0700

    Revert "stdatomic_test: always use bits/stdatomic.h"
    This reverts commit 32b5f4e16b8aec587dc84dcf26c82125839b9a31 to make
    the real revert apply cleanly.
    
    Bug: https://github.com/android/ndk/issues/1178
    Test: None

--- tests/stdatomic_test.cpp
+++ tests/stdatomic_test.cpp
@@ -16,9 +16,12 @@
 
 #include <gtest/gtest.h>
 
-// The real <stdatomic.h> checks for the availability of C++'s atomics and uses them if present. Since
-// we want to test the libc versions, we instead include <bits/stdatomic.h> where they're actually defined.
+#if defined(__ANDROID__)
 #include <bits/stdatomic.h>
+#else
+#undef _USING_LIBCXX  //TODO(b/137876753): Remove this
+#include <stdatomic.h>
+#endif
 
 #include <pthread.h>
 #include <stdint.h>
