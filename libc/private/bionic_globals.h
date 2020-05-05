spsforks/android-bionic-libc

Introduce a new heap tagging level, M_HEAP_TAGGING_LEVEL_SYNC.
The SYNC tagging level enables stack trace collection for allocations and
deallocations, which allows allocation and deallocation stack traces to
appear in tombstones when encountering a tag check fault in synchronous tag
checking mode.

Bug: 135772972
Change-Id: Ibda9f51b29d2c8e2c993fc74425dea7bfa23ab1e

libc/private/bionic_globals.h
@@ -103,6 +103,9 @@ struct libc_shared_globals {
 
   const gwp_asan::AllocatorState *gwp_asan_state = nullptr;
   const gwp_asan::AllocationMetadata *gwp_asan_metadata = nullptr;
+
+  const char *scudo_stack_depot = nullptr;
+  const char *scudo_region_info = nullptr;
 };
 
 __LIBC_HIDDEN__ libc_shared_globals* __libc_shared_globals();

aosp-mirror/platform_bionic

libc/private/bionic_globals.h
@@ -103,6 +103,9 @@ struct libc_shared_globals {
 
   const gwp_asan::AllocatorState* gwp_asan_state = nullptr;
   const gwp_asan::AllocationMetadata* gwp_asan_metadata = nullptr;
+
+  const char* scudo_stack_depot = nullptr;
+  const char* scudo_region_info = nullptr;
 };
 
 __LIBC_HIDDEN__ libc_shared_globals* __libc_shared_globals();