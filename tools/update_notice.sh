ffa5cbeb8654007d2d57a2a6891da03983e8c885
Dan Albert <danalbert@google.com>
2021-02-03 16:44:37 -0800

    Convert generate-NOTICE.py to Python 3, fix name.
    Python module names should be lower case and not use hyphens (the
    former is a convention, the latter is a requirement for importable
    modules).
    
    Also updates the shell script to always use Python 3 so we don't need
    to maintain Python 2 compatibility.
    
    Test: repo upload, in both a python 2 and python 3 virtualenv
    Bug: None
    Change-Id: I486e54a12686b4e528dc6c9c47af5c7a52a7b790

--- tools/update_notice.sh
+++ tools/update_notice.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
-./libc/tools/generate-NOTICE.py libc libm > libc/NOTICE
+python3 ./libc/tools/generate_notice.py libc libm > libc/NOTICE
+if [ $? -ne 0 ]; then
+  >&2 echo NOTICE file generation failed
+  exit 1
+fi
 
 git diff --exit-code HEAD libc/NOTICE
 exit $?
