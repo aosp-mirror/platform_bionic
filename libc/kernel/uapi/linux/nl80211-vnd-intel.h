2abfa9e4f80279dba06a27d90b96b55837be6d4a
Christopher Ferris <cferris@google.com>
2021-11-01 16:26:06 -0700

    Update to v5.15 kernel headers.
    Kernel headers coming from:
    
    Git: https://android.googlesource.com/kernel/common/
    Branch: android-mainline
    Tag: android-mainline-5.15
    
    Test: Builds, and ran bionic unit tests on flame.
    Change-Id: I86901ea6d89fb0f7384b90a47bef35d194083c24

--- libc/kernel/uapi/linux/nl80211-vnd-intel.h
+++ libc/kernel/uapi/linux/nl80211-vnd-intel.h
@@ -16,27 +16,34 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#ifndef __LINUX_TC_SKBMOD_H
-#define __LINUX_TC_SKBMOD_H
-#include <linux/pkt_cls.h>
-#define SKBMOD_F_DMAC 0x1
-#define SKBMOD_F_SMAC 0x2
-#define SKBMOD_F_ETYPE 0x4
-#define SKBMOD_F_SWAPMAC 0x8
-#define SKBMOD_F_ECN 0x10
-struct tc_skbmod {
-  tc_gen;
-  __u64 flags;
+#ifndef __VENDOR_CMD_INTEL_H__
+#define __VENDOR_CMD_INTEL_H__
+#define INTEL_OUI 0x001735
+enum iwl_mvm_vendor_cmd {
+  IWL_MVM_VENDOR_CMD_GET_CSME_CONN_INFO = 0x2d,
+  IWL_MVM_VENDOR_CMD_HOST_GET_OWNERSHIP = 0x30,
+  IWL_MVM_VENDOR_CMD_ROAMING_FORBIDDEN_EVENT = 0x32,
 };
-enum {
-  TCA_SKBMOD_UNSPEC,
-  TCA_SKBMOD_TM,
-  TCA_SKBMOD_PARMS,
-  TCA_SKBMOD_DMAC,
-  TCA_SKBMOD_SMAC,
-  TCA_SKBMOD_ETYPE,
-  TCA_SKBMOD_PAD,
-  __TCA_SKBMOD_MAX
+enum iwl_vendor_auth_akm_mode {
+  IWL_VENDOR_AUTH_OPEN,
+  IWL_VENDOR_AUTH_RSNA = 0x6,
+  IWL_VENDOR_AUTH_RSNA_PSK,
+  IWL_VENDOR_AUTH_SAE = 0x9,
+  IWL_VENDOR_AUTH_MAX,
 };
-#define TCA_SKBMOD_MAX (__TCA_SKBMOD_MAX - 1)
+enum iwl_mvm_vendor_attr {
+  __IWL_MVM_VENDOR_ATTR_INVALID = 0x00,
+  IWL_MVM_VENDOR_ATTR_VIF_ADDR = 0x02,
+  IWL_MVM_VENDOR_ATTR_ADDR = 0x0a,
+  IWL_MVM_VENDOR_ATTR_SSID = 0x3d,
+  IWL_MVM_VENDOR_ATTR_STA_CIPHER = 0x51,
+  IWL_MVM_VENDOR_ATTR_ROAMING_FORBIDDEN = 0x64,
+  IWL_MVM_VENDOR_ATTR_AUTH_MODE = 0x65,
+  IWL_MVM_VENDOR_ATTR_CHANNEL_NUM = 0x66,
+  IWL_MVM_VENDOR_ATTR_BAND = 0x69,
+  IWL_MVM_VENDOR_ATTR_COLLOC_CHANNEL = 0x70,
+  IWL_MVM_VENDOR_ATTR_COLLOC_ADDR = 0x71,
+  NUM_IWL_MVM_VENDOR_ATTR,
+  MAX_IWL_MVM_VENDOR_ATTR = NUM_IWL_MVM_VENDOR_ATTR - 1,
+};
 #endif
