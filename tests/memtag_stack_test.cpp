f5f4112b07fbf011aae1064417b4b84287042c25
Florian Mayer <fmayer@google.com>
2022-08-05 15:21:07 -0700

    [MTE] test for exception cleanup.
    This depends on the LLVM change https://reviews.llvm.org/D135639.
    
    Test: adb shell /data/local/tmp/stack_tagging_helper exception_cleanup
    Bug: 174878242
    Change-Id: Ia1dfdbe482b40c174acaf6c1ac4ad054470d10b8

--- tests/memtag_stack_test.cpp
+++ tests/memtag_stack_test.cpp
@@ -44,13 +44,13 @@ TEST_P(MemtagStackTest, test) {
 #endif
 }
 
-INSTANTIATE_TEST_SUITE_P(, MemtagStackTest,
-                         testing::Combine(testing::Values("vfork_execve", "vfork_execl",
-                                                          "vfork_exit", "longjmp",
-                                                          "longjmp_sigaltstack", "android_mallopt"),
-                                          testing::Bool()),
-                         [](const ::testing::TestParamInfo<MemtagStackTest::ParamType>& info) {
-                           std::string s = std::get<0>(info.param);
-                           if (std::get<1>(info.param)) s += "_static";
-                           return s;
-                         });
+INSTANTIATE_TEST_SUITE_P(
+    , MemtagStackTest,
+    testing::Combine(testing::Values("vfork_execve", "vfork_execl", "vfork_exit", "longjmp",
+                                     "longjmp_sigaltstack", "android_mallopt", "exception_cleanup"),
+                     testing::Bool()),
+    [](const ::testing::TestParamInfo<MemtagStackTest::ParamType>& info) {
+      std::string s = std::get<0>(info.param);
+      if (std::get<1>(info.param)) s += "_static";
+      return s;
+    });
