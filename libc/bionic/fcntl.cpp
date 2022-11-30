spsforks/android-bionic-libc

Tell people when they've messed up with fcntl(FD_SETFD).
This is a subtle bug that even experts struggle with.

Test: treehugger
Change-Id: If9cf16a1c32c836f5688bb3374cfd21d55125b17

libc/bionic/fcntl.cpp
@@ -30,6 +30,7 @@
 #include <fcntl.h>
 
 #include "private/bionic_fdtrack.h"
+#include "private/bionic_fortify.h"
 
 #if defined(__LP64__)
 
@@ -43,6 +44,10 @@ int fcntl(int fd, int cmd, ...) {
   // supported 64-bit ABIs pass arg in a register.
   void* arg = va_arg(args, void*);
   va_end(args);
+
+  if (cmd == F_SETFD && (reinterpret_cast<uintptr_t>(arg) & ~FD_CLOEXEC) != 0) {
+    __fortify_fatal("fcntl(FD_SETFD) passed non-FD_CLOEXEC flag: %p", arg);
+  }
 
   int rc = __fcntl(fd, cmd, arg);
   if (cmd == F_DUPFD) {

aosp-mirror/platform_bionic

libc/bionic/fcntl.cpp
@@ -30,6 +30,7 @@
 #include <fcntl.h>
 
 #include "private/bionic_fdtrack.h"
+#include "private/bionic_fortify.h"
 
 #if defined(__LP64__)
 
@@ -43,6 +44,10 @@ int fcntl(int fd, int cmd, ...) {
   // supported 64-bit ABIs pass arg in a register.
   void* arg = va_arg(args, void*);
   va_end(args);
+
+  if (cmd == F_SETFD && (reinterpret_cast<uintptr_t>(arg) & ~FD_CLOEXEC) != 0) {
+    __fortify_fatal("fcntl(F_SETFD) passed non-FD_CLOEXEC flag: %p", arg);
+  }
 
   int rc = __fcntl(fd, cmd, arg);
   if (cmd == F_DUPFD) {