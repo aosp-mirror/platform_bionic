17e4068bda37bb05a59637c601c0e35d66545199
Elliott Hughes <enh@google.com>
2024-02-13 16:32:29 -0800

    Move three legacy .c files to .cpp.
    Test: treehugger
    Change-Id: Ic22896d25881fe35f28bad53c5412e3e1d4b2661

--- libc/bionic/sched_cpucount.cpp
+++ libc/bionic/sched_cpucount.cpp
@@ -25,19 +25,13 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-#define _GNU_SOURCE 1
+
 #include <sched.h>
-#include <stdlib.h>
 
-cpu_set_t* __sched_cpualloc(size_t count)
-{
-    // The static analyzer complains that CPU_ALLOC_SIZE eventually expands to
-    // N * sizeof(unsigned long), which is incompatible with cpu_set_t. This is
-    // on purpose.
-    return (cpu_set_t*) malloc(CPU_ALLOC_SIZE(count)); // NOLINT
-}
-
-void __sched_cpufree(cpu_set_t* set)
-{
-    free(set);
+int __sched_cpucount(size_t setsize, const cpu_set_t* set) {
+  int count = 0;
+  for (size_t i = 0; i < setsize / sizeof(__CPU_BITTYPE); i++) {
+    count += __builtin_popcountl(set->__bits[i]);
+  }
+  return count;
 }
