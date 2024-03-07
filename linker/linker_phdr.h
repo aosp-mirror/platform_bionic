18d68c82696b0b47d0cfbfb4119a092c83a2c360
Kalesh Singh <kaleshsingh@google.com>
2024-03-07 21:25:39 +0000

    Revert "RELAND: bionic: loader: Extend GNU_RELRO protection"
    Revert submission 2966884
    
    Reason for revert: b/328266487
    
    Reverted changes: /q/submissionid:2966884
    
    Bug: 328266487
    (cherry picked from https://android-review.googlesource.com/q/commit:26de64896cd339ec5e811523b375e3b4ade4860d)
    Merged-In: I3e61443302bf7fd8f58c843c9d7dc3c747897959
    Change-Id: I3e61443302bf7fd8f58c843c9d7dc3c747897959

--- linker/linker_phdr.h
+++ linker/linker_phdr.h
@@ -135,7 +135,7 @@ int phdr_table_unprotect_segments(const ElfW(Phdr)* phdr_table, size_t phdr_coun
                                   ElfW(Addr) load_bias, bool should_pad_segments);
 
 int phdr_table_protect_gnu_relro(const ElfW(Phdr)* phdr_table, size_t phdr_count,
-                                 ElfW(Addr) load_bias, bool should_pad_segments);
+                                 ElfW(Addr) load_bias);
 
 int phdr_table_serialize_gnu_relro(const ElfW(Phdr)* phdr_table, size_t phdr_count,
                                    ElfW(Addr) load_bias, int fd, size_t* file_offset);
