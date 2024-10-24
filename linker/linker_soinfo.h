c46d4c2575f7be37679db68d7544d8ba16c227a6
Daniel Chapin <chapin@google.com>
2024-10-24 21:55:27 +0000

    Revert "Implement deterministic MTE globals for dlext RELRO sharing"
    Revert submission 3236258
    
    Reason for revert: b/374452952
    
    Reverted changes: /q/submissionid:3236258
    (cherry picked from https://android-review.googlesource.com/q/commit:2e40a2284f12f0c474539323231893b7b144a67a)
    Staged CLs so I don't have to wait for merges before I CP
    (cherry picked from https://android-review.googlesource.com/q/commit:f0c2b2ee3cd87105d343007bd1f9e1f13f2864f4)
    Merged-In: I4e152e0cd65ed0d259c1cbea3fd2feb42291729d
    Change-Id: I4e152e0cd65ed0d259c1cbea3fd2feb42291729d

--- linker/linker_soinfo.h
+++ linker/linker_soinfo.h
@@ -254,12 +254,12 @@ struct soinfo {
   void call_constructors();
   void call_destructors();
   void call_pre_init_constructors();
-  bool prelink_image(bool deterministic_memtag_globals = false);
+  bool prelink_image();
   bool link_image(const SymbolLookupList& lookup_list, soinfo* local_group_root,
                   const android_dlextinfo* extinfo, size_t* relro_fd_offset);
   bool protect_relro();
 
-  void tag_globals(bool deterministic_memtag_globals);
+  void tag_globals();
   ElfW(Addr) apply_memtag_if_mte_globals(ElfW(Addr) sym_addr) const;
 
   void add_child(soinfo* child);
